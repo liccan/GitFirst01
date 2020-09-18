be9fe11e4176ca08e5804e72b3aa725e446b173e
me <980950627@qq.com>
2020-09-18 20:11:50 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/git/First01.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Version 1.0
+ * @Author:lichengxin
+ * @Date:2020/9/18
+ * @Package_name:com.atguigu.git
+ */
+public class First01 {
+    public static void main(String[] args) {
+        System.out.println("早上好！good morning");
+    }
+}
