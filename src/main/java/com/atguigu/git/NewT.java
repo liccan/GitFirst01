d302c498557e0b0600e4baf61727a907ef883b76
first <liccan>
2020-09-18 20:32:10 +0800

    New

---
+++ src/main/java/com/atguigu/git/NewT.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Version 1.0
+ * @Author:lichengxin
+ * @Date:2020/9/18
+ * @Package_name:com.atguigu.git
+ */
+public class NewT {
+    public static void main(String[] args) {
+        System.out.println("nihaoyazj");
+    }
+}
